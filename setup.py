fbea66ab60107a08fe03072573ab01583831c45d
chiaming yen <chiamingyen@gmail.com>
2014-12-15 20:08:04 +0800

    123

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+#@+leo-ver=5-thin
+#@+node:2015.20141215190615.1749: * @file V:\w15\setup.py
+#@@language python
+#@@tabwidth -4
+#@+others
+#@+node:2015.20141215190615.1751: ** setup declarations
 from setuptools import setup
 
 setup(name='YourAppName',
@@ -6,5 +12,7 @@ setup(name='YourAppName',
       author='Your Name',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=['cherrypy'],
      )
+#@-others
+#@-leo
